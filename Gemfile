3e15cd0a59ddf31d1b5e7f0a335bf520192a1f65
Takuya Tominaga <tom.qq.emt.03tw@gmail.com>
2018-04-11 01:02:17 +0900

    blog page according

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'materialize-sass'
+gem 'rails_admin'
+
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
   resources :blogs
+
+  root to: "homepage#home"
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
